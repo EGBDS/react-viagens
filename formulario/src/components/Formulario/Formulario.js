f1a9d836a6aeec4445f51756eaaad2eb80161b3c
Erick Gabriel <eg654099@gmail.com>
2023-03-11 21:20:47 -0300

    Adicionando o Formulário ao projeto

--- formulario/src/components/Formulario/Formulario.js
+++ formulario/src/components/Formulario/Formulario.js
@@ -1,9 +1,24 @@
+import aviao from '../../assets/aviao.png';
+import './Locais.css';
 
 function Formulario() {
     return (
         <div>
-            <form>
-
+            <div>
+                <img src={ aviao }></img>
+                <h2>Solicite um orçamento</h2>
+            </div>
+            <form method="get" action="a"> 
+            {/* o metodo get envia as informações aparecendo na url, já o post não mostra.... o action é aonde será pesquisada ou armazenada as informações */}
+                <h3>Informe seus dados</h3>
+                <label>Nome: </label>
+                <br></br>
+                <input type="text" id="nome" placeholder="Nome completo"/>
+                <br></br>
+                <label>Email</label>
+                <br></br>
+                <input type="email"/>
+                <input type="submit">Solicitar orçamento</input>
             </form>
         </div>
     )
